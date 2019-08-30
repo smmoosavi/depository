1c9edfe3f4a1c3ba195f78e2910848f52695d46c
vahid <vahid@vahid>
2019-08-31 00:39:34 +0430

    delivery filter

--- depository/apps/reception/filters.py
+++ depository/apps/reception/filters.py
@@ -7,6 +7,27 @@ from depository.apps.reception.models import Delivery
 
 
 class DeliveryFilter(filters.FilterSet):
+    first_name = filters.CharFilter()
+    last_name = filters.CharFilter()
+    country = filters.CharFilter()
+    phone = filters.CharFilter()
+    passport_id = filters.CharFilter()
+
     class Meta:
         model = Delivery
-        fields = ('pilgrim', 'taker', 'giver', 'hash_id',)
+        fields = ()
+
+    def filter_first_name(self, qs, name, value):
+        return qs.filter(pilgrim__first_name=value)
+
+    def filter_last_name(self, qs, name, value):
+        return qs.filter(pilgrim__last_name=value)
+
+    def filter_country(self, qs, name, value):
+        return qs.filter(pilgrim__country=value)
+
+    def filter_phone(self, qs, name, value):
+        return qs.filter(pilgrim__phone=value)
+
+    def filter_passport_id(self, qs, name, value):
+        return qs.filter(pilgrim__passport_id=value)
