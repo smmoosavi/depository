fdf0d55f277bff87c0f53f4f162f73a12c62f5dd
vahid <vahid@vahid>
2019-09-05 16:57:37 +0430

    delivery viewset

--- depository/apps/reception/urls.py
+++ depository/apps/reception/urls.py
@@ -1,10 +1,10 @@
 from rest_framework.routers import DefaultRouter
 
 from depository.apps.reception.views import ReceptionViewSet, ReportViewSet, \
-    BackUpViewSet
+    BackUpViewSet, DeliveryViewSet
 
 router = DefaultRouter()
 router.register('reception', ReceptionViewSet, 'reception')
-router.register('delivery', ReportViewSet, 'delivery')
+router.register('delivery', DeliveryViewSet, 'delivery')
 router.register('report', ReportViewSet, 'report')
 router.register('backup', BackUpViewSet, 'backup')
