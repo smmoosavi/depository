3534263bbeb6aa084842714159029adc2b1cdf21
vahid <vahid@vahid>
2019-10-07 17:25:24 +0330

    fix admin, make smaller badge

--- depository/apps/accounting/models.py
+++ depository/apps/accounting/models.py
@@ -15,8 +15,7 @@ class Pilgrim(models.Model):
     passport_pic = models.ImageField(null=True, blank=True)
 
     def __str__(self):
-        return f
-        '{self.first_name} {self.last_name} {self.country} {self.passport_id}'
+        return f'{self.first_name} {self.last_name} {self.country} {self.passport_id}'
 
     def get_full_name(self):
         if self.first_name:
