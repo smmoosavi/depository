2e32cfadd1f846810316f58b809c1b85506b9ec5
Reza Zeiny <rezazeiny1998@gmail.com>
2021-08-05 17:51:33 +0430

    change admin page for user

--- depository/apps/accounting/admin.py
+++ depository/apps/accounting/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
-from depository.apps.accounting.models import Pilgrim
+from django.contrib.auth.admin import UserAdmin, GroupAdmin
+from django.contrib.auth.models import Group
+
+from depository.apps.accounting.models import Pilgrim, User
 
 
 @admin.register(Pilgrim)
 class PilgrimAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'passport_id', 'country', 'phone']
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    pass
+
+
+@admin.register(Group)
+class GroupAdmin(GroupAdmin):
+    pass
