d9576b65c91855072773b8e4523bff37d62ef9dd
Reza Zeiny <rezazeiny1998@gmail.com>
2021-09-03 18:05:55 +0430

    Update ansible roles for printer change user admin

--- depository/apps/accounting/admin.py
+++ depository/apps/accounting/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import gettext, gettext_lazy as _
 
 # Register your models here.
 from django.contrib.auth.admin import UserAdmin, GroupAdmin
@@ -14,4 +15,12 @@ class PilgrimAdmin(admin.ModelAdmin):
 
 @admin.register(User)
 class UserAdmin(UserAdmin):
-    pass
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        (_('Extra'), {'fields': ('last_depository')}),
+    )
