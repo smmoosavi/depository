9e0a6ac84ae7d4edc51062b2d7069605bcad7837
Reza Zeiny <rezazeiny1998@gmail.com>
2021-09-03 18:09:43 +0430

    change user admin

--- depository/apps/utils/rest.py
+++ depository/apps/utils/rest.py
@@ -22,6 +22,7 @@ class DepositoryPermission(permissions.BasePermission):
             depository_id = int(depository_id)
         except TypeError or ValueError:
             depository_id = None
+        print(request.user.last_depository.id, depository_id)
         if request.user.last_depository.id != depository_id:
             raise GenericAPIException(
                 detail="There is inconsistency in depository selection, please login again", status_code=468
