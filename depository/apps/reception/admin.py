3dfac893a2301ef75056d34584112a3baa7ae39e
Seyyed Morteza Moosavi <se.mo.moosavi@gmail.com>
2022-09-07 15:36:17 +0430

    add __str__ to models

--- depository/apps/reception/admin.py
+++ depository/apps/reception/admin.py
@@ -7,10 +7,24 @@ from depository.apps.reception.models import Delivery, Pack
 @admin.register(Delivery)
 class DeliveryAdmin(admin.ModelAdmin):
     list_display = ['pilgrim', 'taker', 'giver', 'hash_id', 'entered_at', 'exited_at', 'exit_type']
-    search_fields = ['pilgrim__first_name', 'pilgrim__last_name', 'pilgrim__passport_id']
+    search_fields = ['pilgrim__first_name', 'pilgrim__last_name', 'pilgrim__passport_id', 'pilgrim__phone']
     list_filter = ['exit_type']
+    readonly_fields = [
+        'pilgrim',
+        'taker',
+        'giver',
+        'hash_id',
+        'depository',
+        'entered_at',
+        'exited_at',
+        'exit_type',
+    ]
 
 
 @admin.register(Pack)
 class PackAdmin(admin.ModelAdmin):
-    list_display = ['bag_count', 'pram_count', 'suitcase_count', 'cell']
+    list_display = ['cell', 'delivery', 'bag_count', 'pram_count', 'suitcase_count']
+    search_fields = ['delivery__pilgrim__first_name', 'delivery__pilgrim__last_name', 'delivery__pilgrim__passport_id',
+                     'delivery__pilgrim__phone']
+    list_filter = ['cell']
+    readonly_fields = ['bag_count', 'suitcase_count', 'pram_count', 'delivery', 'cell']
